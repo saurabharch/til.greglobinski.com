5885d53acd7b1e6a59b8191dc17cb71cbf0e3bb7
greglobinski <greglobinski@gmail.com>
2018-09-18 23:29:33 +0200

    fix: upgrade theme to 0.0.7

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -25,38 +25,6 @@ import ArrowUpIcon from 'react-feather/dist/icons/arrow-up';
 
 import config from 'content/meta/config';
 
-// if (typeof window !== `undefined`) {
-//   var detectie = function(win) {
-//     if (!win) {
-//       win = window;
-//     }
-//     var ua = win.navigator.userAgent;
-
-//     var msie = ua.indexOf('MSIE ');
-//     if (msie > 0) {
-//       // IE 10 or older => return version number
-//       return parseInt(ua.substring(msie + 5, ua.indexOf('.', msie)), 10);
-//     }
-
-//     var trident = ua.indexOf('Trident/');
-//     if (trident > 0) {
-//       // IE 11 => return version number
-//       var rv = ua.indexOf('rv:');
-//       return parseInt(ua.substring(rv + 3, ua.indexOf('.', rv)), 10);
-//     }
-
-//     var edge = ua.indexOf('Edge/');
-//     if (edge > 0) {
-//       // IE 12 => return version number
-//       return parseInt(ua.substring(edge + 5, ua.indexOf('.', edge)), 10);
-//     }
-//     // other browser
-//     return false;
-//   };
-
-//   console.log(detectie());
-// }
-
 const sidebarIcons = {
   calendar: CalendarIcon,
   category: FolderIcon,
